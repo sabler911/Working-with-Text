9150d874d840b2c1ce30b5de962abbb8a2d87c55
sabler911 <mr_wolf20011@yahoo.com>
2018-06-18 23:48:04 -0400

    First Comment
    Uploading All files

---
+++ Working with Text/src/Application1.java
@@ -0,0 +1,26 @@
+
+public class Application1 {
+	public static void main(String [] args) {
+		int myInt = 7;
+		
+		String text = "What's up";
+		
+		String blank = " ";
+		
+		String name = "Mike";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		
+		System.out.println("Hello" + " " + "Mike");
+		
+		System.out.println("My integer is: " + myInt);
+		
+		double myDouble = 9.8;
+		
+		System.out.println("My number is: " + myDouble + ",");
+	}
+
+}
